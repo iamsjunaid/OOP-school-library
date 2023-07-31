7b00cef097661d27f996b47e301052b7aae8baea
Junaid Syed <sjunaid626@gmail.com>
2023-07-31 12:04:16 +0530

    create: Person.rb

---
+++ Person.rb
@@ -0,0 +1,33 @@
+class Person
+  attr_reader :id, :name, :age
+
+  def initialize(age, parent_permission: true, name: "Unknown")
+    @id = generate_id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def age=(new_age)
+    @age = new_age
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  def generate_id
+    rand(1..1000)
+  end
+end
+
