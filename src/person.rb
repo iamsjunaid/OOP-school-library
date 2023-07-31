8b20979d6eb422c24ce18c70cd2ae462c7bf1711
Junaid Syed <sjunaid626@gmail.com>
2023-07-31 17:51:23 +0530

    Create person.rb

---
+++ src/person.rb
@@ -0,0 +1,32 @@
+require_relative 'decorator/nameable'
+
+class Person < Nameable
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(age, parent_permission: true, name: 'Unknown')
+    super()
+    @id = generate_id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  def generate_id
+    rand(1..1000)
+  end
+end
