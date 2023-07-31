76fde6054767d4d3389848e8eb54a58b3766d9e4
Junaid Syed <sjunaid626@gmail.com>
2023-07-31 12:06:50 +0530

    create: Student.rb

---
+++ Student.rb
@@ -0,0 +1,10 @@
+class Student < Person
+  def initialize(age, classroom, parent_permission: true, name: "Unknown")
+    super(age, parent_permission: parent_permission, name: name)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
