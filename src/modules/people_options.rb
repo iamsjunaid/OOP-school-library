bdbb211d4c5476c27e8f2f2f8b7abdf230928b16
Nahid1911 <nahid.raihan@gmail.com>
2023-08-07 18:22:06 +0600

    Add refactored People class

---
+++ src/modules/people_options.rb
@@ -0,0 +1,59 @@
+require_relative '../entities/person'
+require_relative '../entities/student'
+require_relative '../entities/teacher'
+
+class PeopleOptions
+  attr_accessor :people_list
+
+  def initialize
+    @people_list = []
+  end
+
+  def list_all_people
+    if @people_list.empty?
+      puts 'No record found! Add a person...'
+    else
+      puts 'All people in the library'
+      @people_list.each do |person|
+        puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+      end
+    end
+  end
+
+  def create_person
+    print 'Do you want to create a student(1) or a teacher(2)? [Input the number] '
+    person_role = gets.chomp
+    case person_role
+    when '1'
+      create_student
+    when '2'
+      create_teacher
+    else
+      puts 'Please add a valid input!'
+    end
+  end
+
+  def create_student
+    print 'Age: '
+    age = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp
+    print 'Has parent permission? [Y/N]: '
+    parent_permission = gets.chomp.downcase == 'y'
+    print 'Classroom: '
+    classroom = gets.chomp.to_i
+    @people_list.push(Student.new(age, classroom, name, parent_permission: parent_permission))
+    puts "\nPerson created successfuly"
+  end
+
+  def create_teacher
+    print 'Age: '
+    age = gets.chomp
+    print 'Name: '
+    name = gets.chomp
+    print 'Specialization: '
+    specialization = gets.chomp
+    @people_list.push(Teacher.new(age, specialization, name))
+    puts "\nPerson created successfuly"
+  end
+end
