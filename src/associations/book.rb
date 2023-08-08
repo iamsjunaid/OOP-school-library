c5a4655771bfb2a1ddb859a3cf9902adc6507cd2
Junaid Syed <sjunaid626@gmail.com>
2023-08-08 16:33:48 +0530

    resolve linter errors

--- src/associations/book.rb
+++ src/associations/book.rb
@@ -13,16 +13,16 @@ class Book
     @rentals << rental
   end
 
-  #convert the "Book" object into a JSON representation
+  # convert the "Book" object into a JSON representation
   def to_json(*args)
     {
-      JSON.create_id  => self.class.name,
+      JSON.create_id => self.class.name,
       'title' => @title,
       'author' => @author
     }.to_json(*args)
   end
 
-  #convert the JSON representation of the "Book" object into a "Book" object
+  # convert the JSON representation of the "Book" object into a "Book" object
   def self.json_create(object)
     new(object['title'], object['author'])
   end
