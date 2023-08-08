7b205e41f26e39bc4a7939a718b8516334472b3f
Junaid Syed <sjunaid626@gmail.com>
2023-08-08 16:35:11 +0530

    Modify `app.rb` to save the books and people in the storage

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,7 @@
 require_relative '../src/modules/book_options'
 require_relative '../src/modules/people_options'
 require_relative '../src/modules/rental_options'
-require_relative '../src/modules/storage.rb'
+require_relative '../src/modules/storage'
 
 class App
   def initialize(options)
@@ -9,6 +9,8 @@ class App
     @book_options = BookOptions.new
     @people_options = PeopleOptions.new
     @rentals_list = RentalOptions.new(@book_options, @people_options)
+    @book_options.books_list = Storage.load_data('books')
+    @people_options.people_list = Storage.load_data('Person')
   end
 
   def select_option(user_choice)
@@ -19,6 +21,7 @@ class App
       @people_options.list_all_people
     when '3'
       @people_options.create_person
+      Storage.save_data('person', @people_options.people_list)
     when '4'
       @book_options.create_book
       Storage.save_data('books', @book_options.books_list)
