5f71764dacd158f0081e04bc538148cf5e5b5ffa
Junaid Syed <sjunaid626@gmail.com>
2023-08-08 16:29:39 +0530

    create save and load methods for JSON file

--- src/modules/storage.rb
+++ src/modules/storage.rb
@@ -1,10 +1,17 @@
 class Storage
-  @@FILE_EXTENSION = 'json'
-  @@BASE_URL = './db/'
+  @file_extension = 'json'
+  @base_url = './db/'
 
   def self.save_data(class_name, object)
-    File.open("#{@@BASE_URL}#{class_name}.#{@@FILE_EXTENSION}", "w") do |f| 
-      f.write JSON.generate(object)
-    end
+    File.write("#{@base_url}#{class_name}.#{@file_extension}", JSON.generate(object))
+  end
+
+  def self.load_data(class_name)
+    file_path = "#{@base_url}#{class_name}.#{@file_extension}"
+
+    return [] unless File.exist?(file_path)
+
+    data_from_file = File.read(file_path)
+    JSON.parse(data_from_file, create_additions: true)
   end
 end
